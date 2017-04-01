a46b63678713a2697db758bd46fc5b3e05a980a4
Sheheryar Parvaz <skipper308@hotmail.ca>
2017-04-01 12:37:40 -0400

    Add scripts for basic bar configuration

---
+++ main.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+PATH=.:./block:$PATH
+
+BG="%{B#222222}"
+FG="%{F#DDDDDD}"
+UR="%{B#EEAAAA}%{F#DDDDDD}" # urgent
+
+
+{
+
+### All
+echo -n "$BG$FG"
+
+### Left
+echo -n "%{l}"
+echo -n "<<i3ws -S -s' $BG$FG' "
+echo -n "-f'$BG$FG ' -u$UR -F'%{B#333}%{F#FFF} '>>"
+echo -n "$BG$FG"
+
+### Center
+echo -n "%{c}"
+echo -n " "
+echo -n "<<clock +%Y-%m-%d>>"
+echo -n "   "
+echo -n "<<clock +%H:%M>>"
+
+### Right
+echo -n "%{r}"
+echo -n " "
+echo -n "<<vol>>"
+echo -n " "
+
+echo ""
+
+} | genbar | lemonbar -b -o 1 -f "Noto Sans" -o -1 -f "FontAwesome"
