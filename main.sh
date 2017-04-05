9cf7b91e796e6136549f06c7e11e2fbbcd88981d
Javier Pollak <javi.po.123@gmail.com>
2017-04-04 22:29:02 -0400

    Add genbar2 & Clean genbar original

--- main.sh
+++ main.sh
@@ -14,16 +14,7 @@ echo -n "$BG$FG"
 
 ### Left
 echo -n "%{l}"
-<<<<<<< HEAD
 echo -n "filler..."
-# echo -n "<<i3ws -S -s' $BG$FG' "
-# echo -n "-f'$BG$FG ' -u$UR -F'%{B#333}%{F#FFF} '>>"
-=======
-echo -n "<<i3ws -S -B "
-echo -n "-f '$BG$FG  ' '  $BG$FG' -u '$UR  ' '  $BG$FG' "
-echo -n "-F '%{B#333}%{F#FFF}  ' '  $BG$FG'>>"
->>>>>>> cfae85405ef8710bfff3bb5856b5c3f73799636c
-echo -n "$BG$FG"
 
 ### Center
 echo -n "%{c}"
@@ -35,12 +26,8 @@ echo -n " "
 echo -n "<<vol>>"
 echo -n " "
 
-#echo ""
+echo ""
 
-<<<<<<< HEAD
-} | genbar | lemonbar -b -o 1 -f "Noto Sans" -o -1 -f "FontAwesome" DVI-1
-=======
-} | genbar \
-  | lemonbar -b -o 1 -f "Noto Sans" -o -1 -f "FontAwesome" \
+} | genbar2  \
+  | lemonbar -b -o 1 -f "Noto Sans" -o -1 -f "FontAwesome" DVI-1\
   | sh > /dev/null
->>>>>>> cfae85405ef8710bfff3bb5856b5c3f73799636c
